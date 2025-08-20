75879b544554b9fef2408debfd4d79e11194a15d
UJ4NG <satetahuroti@gmail.com>
2025-08-20 10:36:19 +0700

    BackEnd Data kandidat (get method)

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -7,3 +7,18 @@ const app = express()
 app.use(cors())
 app.use(express.json());
 // bagian database
+
+const db = mysql.createConnection({ // inisiasi jaringan database
+    host : "localhost",
+    user : 'root',
+    password: '',
+    database: 'pemilihan'
+})
+
+app.get('/data_kandidat', (req,res) => { // get data kandidat
+    const sql = "SELECT * FROM kandidat";
+    db.query(sql, (err,data) => {
+        if(err) return res.json(err);
+        return res.json(data);
+    })
+})
