e3b6f4fa6848a02c2ac8fb9407ff562b2a5335f0
UJ4NG <satetahuroti@gmail.com>
2025-08-20 12:18:55 +0700

    Update main.jsx

--- FrontEnd/src/main.jsx
+++ FrontEnd/src/main.jsx
@@ -1,12 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import Navbar from "./Navbar.jsx"
-import Kandidat from "./Kandidat.jsx"
+// main.jsx
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import Navbar from "./Navbar.jsx";
+import Kandidat from "./Kandidat.jsx";
+import Profile from "./Profile.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <Kandidat />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    {/* Tinggal ganti komponen mana yang mau ditampilin */}
+    <App />
+    {/* <Navbar /> */}
+    {/* <Kandidat /> */}
+    {/* <Profile /> */}
+  </React.StrictMode>
+);
