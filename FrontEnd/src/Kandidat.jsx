a365a54e12d3c4250480de52ecbde6a159bc52f6
UJ4NG <satetahuroti@gmail.com>
2025-08-20 10:22:14 +0700

    add kandidat.jsx

---
+++ FrontEnd/src/Kandidat.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+function Kandidat() {
+    const kandidat = [ // Mock Data (Data ril ada di mysql)
+        {
+            Name : "Tiger",
+            Number : 1,
+        },
+        {
+            Name : "Wolf",
+            Number : 2,
+        },
+        {
+            Name : "Lion",
+            Number : 3,
+        },
+    ]
+  return (
+    <div>
+        {kandidat.map (k => (
+            <button key={k.Number}>
+                <h1>{k.Name}</h1>
+            </button>
+        ))}
+    </div>
+  )
+}
+
+export default Kandidat
